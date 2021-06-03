ee2ab76a131dd2f26ddca0e20b702b0287ba82e6
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-03 16:08:41 -0400

    Created p component to have consistent line height throughout app; added p component to test in unfinished header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Paragraph from "../Components/Paragraph";
 
 function Header() {
-
+    return(
+        <header class="row bg-primary p-5 d-flex justify-content-evenly">
+            <div class="col-md-5 text-white lh-sm">
+                <h1>Anisha McDowell</h1>
+                <Paragraph>
+                    <p>Testing line height, testing line height, testing line height. Testing line height, testing line height, testing line height. Testing line height, testing line height, testing line height. </p>
+                </Paragraph>
+            </div>
+            <div class="col-md-3">
+                <img class="image rounded-circle" src="../../public/profile.jpg" alt="Anisha McDowell" />
+            </div>
+        </header>
+    )
 }
 
 export default Header;
