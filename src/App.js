858bf46d13839df9a518302fec27e11fe3d96cc3
Anisha McDowell <anishamcdowell@gmail.com>
2021-06-03 18:03:45 -0400

    Basic carousel component for projects added; UI only, no JS function yet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Nav from "./Components/Nav";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Carousel from "./Components/Carousel";
 
 function App() {
   return (
     <div className="App">
      <Nav />
      <Header />
+     <Carousel />
      <Footer />
     </div>
   );
