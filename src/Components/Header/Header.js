2efd193af9ba8f335560317bfbe7ed9744138b8b
Anisha McDowell <anishamcdowell@gmail.com>
2021-07-28 15:59:56 -0400

    Adjusted header view height

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,7 +11,7 @@ function Header() {
             <div className="header-font">
             <Logo id="logo"/>
             <div className="col-md-6 text-white lh-sm align-self-center justify-content-center" id="header-text-container">
-            <h1 className="h1">nisha McDowell</h1>
+            <h1 className="name" style={{fontSize: '90px', fontWeight: 'bold'}}>nisha McDowell</h1>
             </div>
             </div>
             <div id="img-div">
