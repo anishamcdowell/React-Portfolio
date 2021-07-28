915798148f6b573005f0acc835c5e6305931f019
Anisha McDowell <anishamcdowell@gmail.com>
2021-07-28 15:46:08 -0400

    Basic header done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,21 +3,19 @@ import ProfilePic from "../ProfilePic/ProfilePic";
 import SmallBtn from "../SmallBtn";
 import "./header.css";
 import "../ProfilePic/profilepic.css";
+import Logo from "../Logo/Logo";
 
 function Header() {
     return(
-        <header className="row d-flex justify-content-around bg-gradient p-5" id="header">
+        <header id="header">
+            <div className="header-font">
+            <Logo id="logo"/>
             <div className="col-md-6 text-white lh-sm align-self-center justify-content-center" id="header-text-container">
-                <h1 className="h1">Anisha McDowell</h1>
-                <h2>
-                Fullstack developer based in Charlotte, NC
-                </h2>
-                <div id="small-header-btn">
-                <SmallBtn>More About Me</SmallBtn>
-                </div>
+            <h1 className="h1">nisha McDowell</h1>
             </div>
-            <div className="col-md-4 profile-pic d-flex justify-content-center" id="profile-pic-container">
-                <ProfilePic/>
+            </div>
+            <div id="img-div">
+            <ProfilePic />
             </div>
         </header>
     )
---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logoimg from "../../images/white-logo.png";
+
+const Logo = () => {
+    return (
+            <img src={logoimg} style={{width: '300px', height: '300px'}}/>
+    )
+}
+
+export default Logo;
