8a4a7781ec08e2d4f81b43972dd95753b4f2d478
Anisha McDowell <anishamcdowell@gmail.com>
2021-11-02 00:14:41 -0400

    Branding in header scales 100%

--- src/Components/Brand Heading/BrandHeading.js
+++ src/Components/Brand Heading/BrandHeading.js
@@ -1,22 +1,24 @@
-import React from "react";
-import Logo from "../Logo/Logo";
-import "./brandheading.css";
+import React from 'react';
+import Logo from '../Logo/Logo';
+import './brandheading.css';
 
 const BrandHeading = () => {
   return (
-    <div id="brand-heading-div">
-      <div class="branding">
-      <Logo />
-      <div className="text-container">
-        <h1 className="name" id="nisha">
-          nisha
-        </h1>
-        <h1 className="name" id="mcdowell">
-          McDowell
-        </h1>
+    <div id='brand-heading-div'>
+      <div class='branding-grid'>
+        <div className='logo-div'>
+          <Logo />
+        </div>
+        <div className='text-container'>
+          <h1 className='name' id='nisha'>
+            nisha
+          </h1>
+          <h1 className='name' id='mcdowell'>
+            McDowell
+          </h1>
+          <h2 id='description'>Full Stack Web Developer</h2>
         </div>
       </div>
-      <h2 id="description">Full Stack Web Developer</h2>
     </div>
   );
 };
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -3,34 +3,6 @@ import logoimg from '../../images/black-logo.png';
 import './logo.css';
 
 const Logo = () => {
-  // const [width, setWidth] = useState(window.innerWidth);
-  // const handleResize = () => {
-  //   setWidth(window.innerWidth);
-  // };
-  // const largebreakpoint = 680;
-  // const smallbreakpoint = 400;
-
-  // useEffect(() => {
-  //   window.addEventListener('resize', handleResize);
-  //   return () => window.removeEventListener('resize', handleResize);
-  // }, [width]);
-
-  // let logowidth;
-  // let logoheight;
-
-  // if (width <= largebreakpoint) {
-  //   logowidth = '80%';
-  //   logoheight = '80%';
-  // } else {
-  //   logowidth = '80%';
-  //   logoheight = '80%';
-  // }
-
-  // if (width <= smallbreakpoint) {
-  //   logowidth = '60%';
-  //   logoheight = '60%';
-  // }
-
   return (
     <div
       id='logo-div'
