bf85e4120fb1b6cf4f30c17367c9b403de565a07
Anisha McDowell <anishamcdowell@gmail.com>
2021-11-01 21:37:21 -0400

    Changed logo image to be responsive with css %

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,41 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import logoimg from "../../images/black-logo.png";
-import "./logo.css";
+import logoimg from '../../images/black-logo.png';
+import './logo.css';
 
 const Logo = () => {
-    const [width, setWidth] = useState(window.innerWidth);
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    }
-    const largebreakpoint = 680;
-    const smallbreakpoint = 400;
-  
-    useEffect(() => {
-      window.addEventListener('resize', handleResize);
-      return () => window.removeEventListener('resize', handleResize)
-    }, [width])
+  // const [width, setWidth] = useState(window.innerWidth);
+  // const handleResize = () => {
+  //   setWidth(window.innerWidth);
+  // };
+  // const largebreakpoint = 680;
+  // const smallbreakpoint = 400;
 
-    let logowidth;
-    let logoheight;
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, [width]);
 
-    if (width <= largebreakpoint) {
-        logowidth="200px";
-        logoheight="200px";
-    } else {
-        logowidth="300px";
-        logoheight="300px";
-    }
+  // let logowidth;
+  // let logoheight;
 
-    if (width <= smallbreakpoint) {
-      logowidth="100px";
-      logoheight="100px";
-    }
+  // if (width <= largebreakpoint) {
+  //   logowidth = '80%';
+  //   logoheight = '80%';
+  // } else {
+  //   logowidth = '80%';
+  //   logoheight = '80%';
+  // }
 
-    return (
-        <div id="logo-div" style={{width: `${logowidth}`, height: `${logoheight}`}}>
-            <img id="logo" src={logoimg} style={{width: `${logowidth}`, height: `${logoheight}`}}/>
-            </div>
-    )
-}
+  // if (width <= smallbreakpoint) {
+  //   logowidth = '60%';
+  //   logoheight = '60%';
+  // }
+
+  return (
+    <div
+      id='logo-div'
+      // style={{ width: `${logowidth}`, height: `${logoheight}` }}
+    >
+      <img
+        id='logo'
+        src={logoimg}
+        // style={{ width: `${logowidth}`, height: `${logoheight}` }}
+      />
+    </div>
+  );
+};
 
 export default Logo;
