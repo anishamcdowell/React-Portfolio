281e860df18d520cfcbf4c4781b1d46a9bd25c08
Anisha McDowell <anishamcdowell@gmail.com>
2021-11-06 14:16:54 -0400

    Footer style updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,8 @@ import "./footer.css";
 
 function Footer() {
     return(
-        <footer className="footer mb-auto py-4 bg-dark bg-gradient d-flex flex-column justify-content-evenly align-items-center" id="contact">
+        <footer className="footer mb-auto py-4 d-flex flex-column justify-content-evenly align-items-center" id="contact">
+            <p>Currently seeking work and open to good conversation.</p>
           <div id="icons-container">
       <ul id="socials-list">
           <li className="social-li">
